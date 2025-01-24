a2e54a42fcb50dfc60cb8f6d8eb26717c4a4e2ca
theo-mi <mintheonz@gmail.com>
2025-01-25 02:24:47 +0900

    feat: 상품 서비스 추가 및 카테고리 내 최저가 상품 조회 메서드 추가
    - 동일한 최저가 상품이 있는경우 마지막 상품을 반환한다.

---
+++ src/main/java/com/mms/product/service/ProductService.java
@@ -0,0 +1,31 @@
+package com.mms.product.service;
+
+import com.mms.product.model.Category;
+import com.mms.product.model.Product;
+import com.mms.product.repository.ProductRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+  private final ProductRepository productRepository;
+
+  /**
+   * 카테고리에서 최저가 상품을 조회한다.
+   *
+   * @param category 카테고리
+   * @return 최저가 상품
+   */
+  public Product getCheapestByCategory(Category category) {
+    final List<Product> minPriceProductsByCategory = productRepository.findMinPriceProductsByCategory(category.getId());
+
+    // 중복된 최저가 상품이 있는 경우 마지막 상품을 반환한다. (예시에 따라 수정)
+    // 현재는 별다른 정책이 없기 때문에 limit 1으로 가져오도록 수정하면 조회 성능이 더 좋아질듯.
+    return minPriceProductsByCategory.getLast();
+  }
+}
