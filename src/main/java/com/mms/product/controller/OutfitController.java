8c647d0dc6019e348982abd13b1374555212d465
theo-mi <mintheonz@gmail.com>
2025-01-26 01:58:27 +0900

    fix: outfit, price 컨트롤러 매핑 url 추가
    - Controller에 붙여놓고 왜 url이 안붙지 하고있었음.....

--- src/main/java/com/mms/product/controller/OutfitController.java
+++ src/main/java/com/mms/product/controller/OutfitController.java
@@ -7,9 +7,11 @@ import com.mms.product.service.OutfitService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("outfit")
+@RestController
+@RequestMapping("outfits")
 @RequiredArgsConstructor
 public class OutfitController {
 
--- src/main/java/com/mms/product/controller/PriceController.java
+++ src/main/java/com/mms/product/controller/PriceController.java
@@ -8,9 +8,11 @@ import com.mms.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("prices")
+@RestController
+@RequestMapping("prices")
 @RequiredArgsConstructor
 public class PriceController {
 
