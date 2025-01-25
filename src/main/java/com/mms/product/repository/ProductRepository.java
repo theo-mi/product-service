80f78772a86c8f0ff802bdf4a5eb9350655dabcd
theo-mi <mintheonz@gmail.com>
2025-01-25 15:36:23 +0900

    feat: 카테고리와 브랜드 기준 최저가 상품을 조회하는 로직 추가

--- src/main/java/com/mms/product/repository/ProductRepository.java
+++ src/main/java/com/mms/product/repository/ProductRepository.java
@@ -15,6 +15,23 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
    * @param categoryId 카테고리 ID
    * @return 최저가 상품 목록
    */
-  @Query("SELECT p FROM Product p WHERE p.category.id = :categoryId AND p.price = (SELECT MIN(p2.price) FROM Product p2 WHERE p2.category.id = :categoryId)")
+  @Query("SELECT p "
+      + "FROM Product p "
+      + "WHERE p.category.id = :categoryId "
+      + "AND p.price = (SELECT MIN(p2.price) FROM Product p2 WHERE p2.category.id = :categoryId)")
   List<Product> findMinPriceProductsByCategory(Long categoryId);
+
+  /**
+   * 카테고리와 브랜드에 해당하는 최저가 상품을 조회한다.
+   *
+   * @param categoryId 카테고리 ID
+   * @param brandId    브랜드 ID
+   * @return 최저가 상품 목록
+   */
+  @Query("SELECT p "
+      + "FROM Product p "
+      + "WHERE p.category.id = :categoryId "
+      + "AND p.brand.id = :brandId "
+      + "AND p.price = (SELECT MIN(p2.price) FROM Product p2 WHERE p2.category.id = :categoryId AND p2.brand.id = :brandId)")
+  List<Product> findMinPriceProductByBrandAndCategory(Long categoryId, Long brandId);
 }
--- src/main/java/com/mms/product/service/ProductService.java
+++ src/main/java/com/mms/product/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.mms.product.service;
 
+import com.mms.product.model.Brand;
 import com.mms.product.model.Category;
 import com.mms.product.model.Product;
 import com.mms.product.repository.ProductRepository;
@@ -27,5 +28,18 @@ public class ProductService {
     // 중복된 최저가 상품이 있는 경우 마지막 상품을 반환한다. (예시에 따라 수정)
     // 현재는 별다른 정책이 없기 때문에 limit 1으로 가져오도록 수정하면 조회 성능이 더 좋아질듯.
     return minPriceProductsByCategory.getLast();
+  }
+
+  /**
+   * 카테고리와 브랜드에 해당하는 최저가 상품을 조회한다.
+   *
+   * @param brand    브랜드
+   * @param category 카테고리
+   * @return 최저가 상품
+   */
+  public Product getCheapestBy(Brand brand, Category category) {
+    final List<Product> minPriceProductByBrandAndCategory = productRepository.findMinPriceProductByBrandAndCategory(category.getId(), brand.getId());
+
+    return minPriceProductByBrandAndCategory.getLast();
   }
 }
