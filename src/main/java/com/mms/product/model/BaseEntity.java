219f1b142f56646460f7c02e0163ac4f4ac9c64c
theo-mi <mintheonz@gmail.com>
2025-01-26 01:54:56 +0900

    feat: 등록, 수정 일자를 보유하는 기본 참조 엔티티 추가

---
+++ src/main/java/com/mms/product/model/BaseEntity.java
@@ -0,0 +1,19 @@
+package com.mms.product.model;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+}
