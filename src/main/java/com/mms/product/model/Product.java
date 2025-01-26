6aa5538b7b3b77c423720699a172e522533b676b
theo-mi <mintheonz@gmail.com>
2025-01-26 14:16:39 +0900

    feat: 상품 entity를 수정하거나 생성할 수 있는 팩터리 메서드 추가

--- src/main/java/com/mms/product/model/Product.java
+++ src/main/java/com/mms/product/model/Product.java
@@ -7,10 +7,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.math.BigDecimal;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,4 +29,34 @@ public class Product {
   private Brand brand;
 
   private BigDecimal price;
+
+  @Builder(access = AccessLevel.PRIVATE)
+  public Product(Long id, Category category, Brand brand, BigDecimal price) {
+    this.id = id;
+    this.category = category;
+    this.brand = brand;
+    this.price = price;
+  }
+
+  public static Product of(Category category, Brand brand, BigDecimal price) {
+    return Product.builder()
+        .category(category)
+        .category(category)
+        .brand(brand)
+        .price(price)
+        .build();
+  }
+
+  /**
+   * 상품 정보를 수정한다.
+   *
+   * @param category 카테고리
+   * @param brand    브랜드
+   * @param price    가격
+   */
+  public void update(Category category, Brand brand, BigDecimal price) {
+    this.category = category;
+    this.brand = brand;
+    this.price = price;
+  }
 }
