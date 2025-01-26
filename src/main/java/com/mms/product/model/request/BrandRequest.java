54bb3806c9ff9910c013bf757f553badefec0b92
theo-mi <mintheonz@gmail.com>
2025-01-26 15:07:39 +0900

    doc: 브랜드 등록, 상품 등록시 예시 model 설정 및 설명 추가

--- src/main/java/com/mms/product/model/request/BrandRequest.java
+++ src/main/java/com/mms/product/model/request/BrandRequest.java
@@ -1,9 +1,12 @@
 package com.mms.product.model.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
+@Schema(description = "브랜드 등록시 요청하는 정보")
 @Getter
 public class BrandRequest {
 
+  @Schema(description = "브랜드명", example = "나이키")
   private String name;
 }
--- src/main/java/com/mms/product/model/request/ProductRequest.java
+++ src/main/java/com/mms/product/model/request/ProductRequest.java
@@ -1,13 +1,19 @@
 package com.mms.product.model.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
 import lombok.Getter;
 
+@Schema(description = "상품 등록시 요청하는 정보")
 @Getter
 public class ProductRequest {
+
+  @Schema(description = "카테고리 ID", example = "1")
   private Long categoryId;
-  
+
+  @Schema(description = "브랜드 ID", example = "1")
   private Long brandId;
 
+  @Schema(description = "상품가격", example = "52300")
   private BigDecimal price;
 }
