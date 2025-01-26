c010a2dde9a014958c9c69b0d2d6f3a233f62c12
theo-mi <mintheonz@gmail.com>
2025-01-27 02:10:58 +0900

    fix: 누락되었던 Brand쪽 repository 어노테이션 추가

--- src/main/java/com/mms/product/repository/BrandRepository.java
+++ src/main/java/com/mms/product/repository/BrandRepository.java
@@ -2,7 +2,9 @@ package com.mms.product.repository;
 
 import com.mms.product.model.entity.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BrandRepository extends JpaRepository<Brand, Long> {
 
 }
