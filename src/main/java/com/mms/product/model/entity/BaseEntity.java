53b48b6962ef8ba3a56277a7a38c060e6540f1a6
theo-mi <mintheonz@gmail.com>
2025-01-26 16:59:52 +0900

    feat: 엔티티 디비 등록시 수정시각, 등록시각에 디폴트로 현재시가 들어가도록 처리

--- src/main/java/com/mms/product/model/entity/BaseEntity.java
+++ src/main/java/com/mms/product/model/entity/BaseEntity.java
@@ -3,6 +3,8 @@ package com.mms.product.model.entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Comment;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,9 +13,13 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
+  @ColumnDefault(value = "CURRENT_TIMESTAMP")
   @CreatedDate
+  @Comment("등록 시각")
   private LocalDateTime createdAt;
 
+  @ColumnDefault(value = "CURRENT_TIMESTAMP")
   @LastModifiedDate
+  @Comment("수정 시각")
   private LocalDateTime updatedAt;
 }
