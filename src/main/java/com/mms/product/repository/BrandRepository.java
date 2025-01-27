8b398ca83a8690e21a791ff4ce07c9666aff3d8e
theo-mi <mintheonz@gmail.com>
2025-01-27 18:44:47 +0900

    refactor: 카테고리별 최저,최고가에 사용하는 서브쿼리들을 cte로 빼서 살짝 리팩터링

--- src/main/java/com/mms/product/repository/BrandRepository.java
+++ src/main/java/com/mms/product/repository/BrandRepository.java
@@ -13,7 +13,12 @@ import org.springframework.stereotype.Repository;
 public interface BrandRepository extends JpaRepository<Brand, Long> {
 
   @Query(value = """
-      WITH brand_category_min_price AS (
+      WITH category_count AS (
+          SELECT COUNT(c.id) AS total_categories
+          FROM category c
+          WHERE c.id IN (:categoryIds)
+      ),
+      brand_category_min_price AS (
           SELECT
               p.brand_id         AS brand_id,
               p.category_id      AS category_id,
@@ -21,19 +26,23 @@ public interface BrandRepository extends JpaRepository<Brand, Long> {
           FROM product p
           WHERE p.category_id IN (:categoryIds)
           GROUP BY p.brand_id, p.category_id
+      ),
+      brand_total_price AS (
+          SELECT
+              bcmp.brand_id       AS brand_id,
+              SUM(bcmp.min_price) AS total_price,
+              COUNT(bcmp.category_id) AS covered_categories
+          FROM brand_category_min_price bcmp
+          GROUP BY bcmp.brand_id
       )
       SELECT
-          bcmp.brand_id       AS brandId,
-          SUM(bcmp.min_price) AS totalPrice
-      FROM brand_category_min_price bcmp
-      GROUP BY bcmp.brand_id
-      HAVING COUNT(bcmp.category_id) = (
-        SELECT COUNT(c.id)
-        FROM category c
-        WHERE c.id IN (:categoryIds)
-      )
-      ORDER BY SUM(bcmp.min_price)
-      LIMIT 1
+          btp.brand_id       AS brandId,
+          btp.total_price    AS totalPrice
+      FROM brand_total_price btp
+      JOIN category_count cc
+        ON btp.covered_categories = cc.total_categories
+      ORDER BY btp.total_price
+      LIMIT 1;
       """,
       nativeQuery = true)
   Optional<CheapestBrandDto> findCheapestBrandWithCategories(@Param("categoryIds") List<Long> categoryIds);
