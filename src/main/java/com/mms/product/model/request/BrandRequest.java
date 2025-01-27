68c47083c6487dddaecb4b091a394da343c1d522
theo-mi <mintheonz@gmail.com>
2025-01-27 16:32:59 +0900

    fix: 문자용 길이 제한 validation으로 변경 (기존엔 숫자용이었음..)

--- src/main/java/com/mms/product/model/request/BrandRequest.java
+++ src/main/java/com/mms/product/model/request/BrandRequest.java
@@ -1,8 +1,8 @@
 package com.mms.product.model.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Schema(description = "브랜드 등록시 요청하는 정보")
@@ -11,6 +11,6 @@ public class BrandRequest {
 
   @Schema(description = "브랜드명", example = "나이키")
   @NotBlank(message = "브랜드명은 필수입니다.")
-  @Max(value = 100, message = "브랜드명은 100자 이하여야 합니다.")
+  @Size(min = 2, max = 100, message = "브랜드명은 100자 이하여야 합니다.")
   private String name;
 }
