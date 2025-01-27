b1fffaf9704c6af53764a3108feaf59953e7665f
theo-mi <mintheonz@gmail.com>
2025-01-27 14:21:03 +0900

    fix: brand, product request 제약 조건 추가

--- src/main/java/com/mms/product/model/request/BrandRequest.java
+++ src/main/java/com/mms/product/model/request/BrandRequest.java
@@ -1,6 +1,7 @@
 package com.mms.product.model.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
@@ -10,5 +11,6 @@ public class BrandRequest {
 
   @Schema(description = "브랜드명", example = "나이키")
   @NotBlank(message = "브랜드명은 필수입니다.")
+  @Max(value = 100, message = "브랜드명은 100자 이하여야 합니다.")
   private String name;
 }
--- src/main/java/com/mms/product/model/request/ProductRequest.java
+++ src/main/java/com/mms/product/model/request/ProductRequest.java
@@ -2,6 +2,7 @@ package com.mms.product.model.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Getter;
@@ -11,14 +12,17 @@ import lombok.Getter;
 public class ProductRequest {
 
   @Schema(description = "카테고리 ID", example = "1")
-  @Positive(message = "카테고리 ID는 필수입니다.")
+  @NotNull(message = "카테고리 ID는 필수입니다.")
+  @Positive(message = "카테고리 ID는 1 이상이어야 합니다.")
   private Long categoryId;
 
   @Schema(description = "브랜드 ID", example = "1")
-  @Positive(message = "브랜드 ID는 필수입니다.")
+  @NotNull(message = "카테고리 ID는 필수입니다.")
+  @Positive(message = "브랜드 ID는 1 이상이어야 합니다.")
   private Long brandId;
 
   @Schema(description = "상품가격", example = "52300")
+  @NotNull(message = "카테고리 ID는 필수입니다.")
   @Min(value = 1, message = "상품 가격은 1원 이상이어야 합니다.")
   private BigDecimal price;
 }
