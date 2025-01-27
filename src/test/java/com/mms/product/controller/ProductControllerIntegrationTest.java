618a7692ea682952dd1fbae775e7390bff5df035
theo-mi <mintheonz@gmail.com>
2025-01-27 23:05:41 +0900

    test: 테스트 전용 yml 파일 설정 하여 불러오도록 처리
    - 테스트 코드는 캐시 사용하지 않도록 처리

--- src/test/java/com/mms/product/controller/ProductControllerIntegrationTest.java
+++ src/test/java/com/mms/product/controller/ProductControllerIntegrationTest.java
@@ -18,12 +18,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@ActiveProfiles("test")
 class ProductControllerIntegrationTest {
 
   @Autowired
